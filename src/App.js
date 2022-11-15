892859ffa509a75d4b5db5043ece7d9331cb5f05
Ankit Patidar <ankitpatidar991@gmail.com>
2022-11-15 19:57:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
 
-function App() {
+const Box = ({ style = {}, index }) => {
+  const bgColor = index % 2 === 0 ? "#fff" : "#000";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ height: 40, width: 40, backgroundColor: bgColor, ...style }}
+    ></div>
+  );
+};
+
+const RenderRow = ({ index, value }) => {
+  let boxComponent = useMemo(() => {
+    let arr = [];
+    for (let i = 0; i < value; i++) {
+      arr.push(<Box key={index * i} index={i + index} />);
+    }
+    return arr;
+  }, [value]);
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>{boxComponent}</div>
+  );
+};
+
+const DrawBox = ({ value }) => {
+  const rowComponent = useMemo(() => {
+    let arr = [];
+    for (let i = 0; i < value; i++) {
+      arr.push(<RenderRow value={value} key={i} index={i} />);
+    }
+    return arr;
+  }, [value]);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {rowComponent}
+    </div>
+  );
+};
+export default function App() {
+  const [value, setValue] = useState(8);
+
+  return (
+    <div>
+      <input
+        value={value}
+        style={{ margin: 10 }}
+        type="number"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <DrawBox value={value} />
     </div>
   );
 }
-
-export default App;
